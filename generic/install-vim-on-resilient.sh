55f55e66aa5c2442da2bf787b5c7c9deb7626875
Ali Okan Yüksel <aokany@gmail.com>
2020-07-08 23:02:20 +0300

    Create install-vim-on-resilient.sh

---
+++ generic/install-vim-on-resilient.sh
@@ -0,0 +1,5 @@
+wget https://rpmfind.net/linux/centos/7.8.2003/os/x86_64/Packages/vim-enhanced-7.4.629-6.el7.x86_64.rpm
+wget https://rpmfind.net/linux/centos/7.8.2003/os/x86_64/Packages/vim-common-7.4.629-6.el7.x86_64.rpm
+wget https://rpmfind.net/linux/centos/7.8.2003/os/x86_64/Packages/vim-filesystem-7.4.629-6.el7.x86_64.rpm
+wget https://rpmfind.net/linux/centos/7.8.2003/os/x86_64/Packages/gpm-libs-1.20.7-6.el7.x86_64.rpm
+rpm -ivh vim-enhanced-7.4.629-6.el7.x86_64.rpm vim-common-7.4.629-6.el7.x86_64.rpm vim-filesystem-7.4.629-6.el7.x86_64.rpm gpm-libs-1.20.7-6.el7.x86_64.rpm 
