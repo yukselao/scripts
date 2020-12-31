54348855d8c2a31726140cd6a7d87b2927f620c4
Ali Okan Yüksel <aokany@gmail.com>
2020-12-31 03:29:45 +0300

    Update and rename get-macaddresses-from-vcenter.sh to fix-dhcpd.sh

--- openshift-installation/fix-dhcpd.sh
+++ openshift-installation/fix-dhcpd.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
 function getmac() {
         govc vm.info -json=true /Datacenter/vm/security/$1 | jq '.VirtualMachines[] | .Config | .Hardware | .Device[] | select( (.DeviceInfo | .Label)=="Network adapter 1")' |jq '.MacAddress'
 
 }
 hosts=(bootstrap-0 control-plane-0 control-plane-1 control-plane-2 compute-0 compute-1 compute-2)
+cp /opt/openshift/config/dhcpd.conf /tmp/dhcpd.conf
 for host in ${hosts[@]}; do
-        echo $host $(getmac $host)
+        macaddr=$(getmac $host |tr -d '"')
+        sed -r -i "s#%""$host""%#""$macaddr""#g" /tmp/dhcpd.conf
 done
+cp /tmp/dhcpd.conf /etc/dhcp/dhcpd.conf
+systemctl restart dhcpd
